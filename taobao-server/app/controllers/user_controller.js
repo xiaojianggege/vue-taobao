e902dffc12f7e94ea084d6116fc5146fd283d9cc
xiaojianggege <2804149747@qq.com>
2020-10-17 20:07:35 +0800

    gengxin

--- taobao-server/app/controllers/user_controller.js
+++ taobao-server/app/controllers/user_controller.js
@@ -5,20 +5,17 @@ const user_col = require('../models/user')
 const password_col = require('../models/password')
 const { v1: uuidv1} = require('uuid') // 引入uuid 给每个注册用户设置一个时间戳的userId
 const md5 = require('md5') // 使用md5给每个用户密码进行加密
-
 // 登录
 const login = async (ctx, next) => {
   
 }
 
 
-
-
-
 // 注册
 const register = async (ctx, next) => {
   const req = ctx.request.body // 获取用户传递来的数据
   // 先对数据库进行查询 是否已经存在要注册的用户名
+  console.log(req);
   const query = await user_col.findOne({
     userName: req.userName
   },{
@@ -28,33 +25,43 @@ const register = async (ctx, next) => {
   ctx.status = 200
   if(query) {
     ctx.body = {
-      status: 0,
+      code: 0,
       msg: '用户名已存在！',
-      data: 'error'
     }
     return
   }
   const useId = uuidv1()
-  const hash = md5(req.password)
-  const addUser_col = user_col.create({
+  const newUser = await user_col.create({
     userId,
     userName: req.userName
   })
-  const addPassword_col =  password_col.create({
-    userId,
-    hash 
-  })
-  if(addUser_col && addPassword_col) {
-    ctx.body = {
-      status: 1,
-      msg: '成功创建用户',
-      data: req.userName
+  // const newUser_password =  password_col.create({
+  //   userId,
+  //   hash 
+  // })
+  if(newUser) {
+    const hash = await md5(req.password)
+    const newPass = await password_col.create({
+       userId,
+       hash 
+    })
+    if(newPass) {
+      ctx.body = {
+        code: 1,
+        msg: '用户注册成功',
+        data: newUser
+      }
+    } else{
+      ctx.body = {
+        code: 0,
+        msg: '密码存储失败！'
+      }
     }
+    
   } else {
     ctx.body = {
-      status: 0,
-      msg: '数据库存储失败！',
-      data: 'error'
+      code: 0,
+      msg: '用户注册失败！'
     }
   }
 
